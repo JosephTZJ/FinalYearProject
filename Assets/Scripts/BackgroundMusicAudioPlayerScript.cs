98e23b982c74820af27653a53a6b5f9bfd7a1b8d
Joseph Tee <zijuntee19@gmail.com>
2024-03-01 22:32:54 +0800

    Version 3.0
    Completed sound setting

--- Assets/Scripts/BackgroundMusicAudioPlayerScript.cs
+++ Assets/Scripts/BackgroundMusicAudioPlayerScript.cs
@@ -2,72 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-// when clicking cross "x" at the top
 using Google.XR.Cardboard;
 using UnityEngine.XR;
 using UnityEngine.XR.Management;
-// namespace BackgroundMusicAudioPlayerScript
-// {
 
 
 public class BackgroundMusicAudioPlayerScript : MonoBehaviour
 {
     public static BackgroundMusicAudioPlayerScript instance;
 
-    /* void Awake()
-    {
-        if (instance != null)
-            Destroy(gameObject);
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-            // BackgroundMusicAudioPlayerScript.instance.GetComponent<AudioSource>().Play();
-        }
-    } */
+    public AudioSource musicSource;
+    public GameObject musicPrefab;
 
     void Start()
     {
-        if (instance != null)
-            Destroy(gameObject);
-        else
+
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
-            // BackgroundMusicAudioPlayerScript.instance.GetComponent<AudioSource>().Play();
         }
-        if (!Api.HasDeviceParams())
+        else
         {
-            Api.ScanDeviceParams();
+            Destroy(gameObject);
+            return;
         }
+
+        if (!Api.HasDeviceParams())
+            Api.ScanDeviceParams();
+
+        if (instance.musicSource == null)
+        {
+            GameObject music = Instantiate(musicPrefab);
+            instance.musicSource = music.GetComponent<AudioSource>();
+        }
+
+        instance.musicSource.volume = PlayerPrefs.GetFloat("musicVolume");
+
     }
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        // }
-
-        // Debug.Log("active scene: "+ SceneManager.GetActiveScene().name);
-
-        if (SceneManager.GetActiveScene().name == "EndStoryScene")
-            BackgroundMusicAudioPlayerScript.instance.GetComponent<AudioSource>().Pause();
-        else if (SceneManager.GetActiveScene().name == "Story2_2ClassroomScene" || SceneManager.GetActiveScene().name == "Story2_1ClassroomScene" || SceneManager.GetActiveScene().name == "Story3_1ClassroomScene" || SceneManager.GetActiveScene().name == "Story3_2PlaygroundScene")
+        if (SceneManager.GetActiveScene().name == "EndStoryScene" || SceneManager.GetActiveScene().name == "ParentChooseEnvironmentScene")
+            musicSource.Pause();
+        else if (SceneManager.GetActiveScene().name == "AudioSettingScene" || SceneManager.GetActiveScene().name == "Story2_2ClassroomScene" || SceneManager.GetActiveScene().name == "Story2_1ClassroomScene" || SceneManager.GetActiveScene().name == "Story3_1ClassroomScene" || SceneManager.GetActiveScene().name == "Story3_2PlaygroundScene")
         {
             // Resume the audio
-            BackgroundMusicAudioPlayerScript.instance.GetComponent<AudioSource>().UnPause();
+            musicSource.UnPause();
         }
 
         if (Api.IsCloseButtonPressed)
         {
-            BackgroundMusicAudioPlayerScript.instance.GetComponent<AudioSource>().Pause();
+            musicSource.Pause();
         }
-        // else
-        //     BackgroundMusicAudioPlayerScript.instance.GetComponent<AudioSource>().Play();
-
-        //BGmusic.instance.GetComponent<AudioSource>().Play();
-
+       
     }
 
 }
-// }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    public AudioSource musicSource;
+    void Start()
+    {
+        musicSource = GameObject.Find("Happy-and-fun-background-music").GetComponent<AudioSource>();
+
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        musicSource.volume = volumeSlider.value;
+
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
